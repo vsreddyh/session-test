1e8e2d3c48a0d0dc8c58a8da5e76475c3e691efd
vsreddyh <shouryanreddyh@gmail.com>
2024-06-17 14:51:23 +0530

    x

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const session = require('express-session');
-const bodyParser = require('body-parser');
 const MongoDBStore = require('connect-mongodb-session')(session);
-app.use(cors({
-    origin: 'https://session-test-lac.vercel.app/',
-    credentials: true
-}));
-var store = new MongoDBStore({
-    uri: "mongodb+srv://Project:Florencemidhebaramvesam@project.tbx2krn.mongodb.net/sessy?retryWrites=true&w=majority",
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const store = new MongoDBStore({
+    uri: 'mongodb+srv://Project:Florencemidhebaramvesam@project.tbx2krn.mongodb.net/sessy?retryWrites=true&w=majority',
     collection: 'mySessions',
 });
 
-app.use(
-    session({
-        secret: "9a3jKL$#3jfk4kljg%2f7sJ@*Lmn2J7H",
-        resave: true,
-        store: store,
-        saveUninitialized: false,
-        cookie: {
-            httpOnly: true,
-            sameSite: true,
-            secure: true,
-            maxAge: 6 * 60 * 60 * 1000, //6 hours
-            rolling: true, //whenever session is modified it resets expirytime
-        },
-    })
-);
+app.use(cors({
+    origin: 'https://session-test-lac.vercel.app',
+    credentials: true
+}));
+
+app.use(session({
+    secret: '9a3jKL$#3jfk4kljg%2f7sJ@*Lmn2J7H',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: true // must be true to send cookies over https
+    }
+}));
+
 app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.json('hello world');
 });
+
 app.get('/store', (req, res) => {
     req.session.store = 'hi0';
     req.session.save(err => {
-        if(err) {
+        if (err) {
             console.log(err);
         }
-        console.log(req.session.store,"hlo");
+        console.log(req.session.store, "hlo");
         res.json('stored');
     });
 });
+
 app.get('/get', (req, res) => {
     console.log(req.session.store);
-    res.json({data:req.session.store,id:req.sessionID,id1:req.session.id,cookie:req.session.cookie});
+    res.json({
+        data: req.session.store,
+        id: req.sessionID,
+        id1: req.session.id,
+        cookie: req.session.cookie
+    });
 });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Running on port ${port}`));
 
